a10ffa484789aebb046e8ea005db991f7297b83b
Andres-R-code <72171067+Andres-R-code@users.noreply.github.com>
2021-07-16 22:41:20 -0500

    permissions

--- moduls/users/permissions.py
+++ moduls/users/permissions.py
@@ -3,17 +3,22 @@ from rest_framework.permissions import BasePermission
 class UserPermissions(BasePermission):
 
     def has_permission(self, request, view,):
-    
-        if request.method == 'GET':
-            return False
+
+        if request.method == 'GET' and request.user.is_staff:
+            return True
+
+        if request.method == 'GET' and request.user.is_authenticated and view.detail:
+            return True 
 
         if request.method == 'POST' and request.user.is_anonymous:
             return True
 
-        return True
+        if request.method in ['DELETE', 'PATCH', 'PUT'] and request.user.is_authenticated:
+            return True
 
-    def has_object_permission(self, request, view, obj):
-        print('has object_permission', view, obj)
+        return False
+
+    def has_object_permission(self, request,view, obj):
 
         if request.method == 'PATCH' and not request.user.is_authenticated:
             return False
@@ -21,6 +26,6 @@ class UserPermissions(BasePermission):
         if request.method == 'DELETE':
             return False
 
-        return super().has_object_permission(request, view, obj)
+        return True
 
 
