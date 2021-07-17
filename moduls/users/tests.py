1a8166266418840d43ac438e565f50ac01270d13
Andres-R-code <72171067+Andres-R-code@users.noreply.github.com>
2021-07-16 22:37:54 -0500

    Users Tests

--- moduls/users/tests.py
+++ moduls/users/tests.py
@@ -11,59 +11,78 @@ class UserTestCase(APITestCase):
         'password': 'password',
         'first_name' : 'User',
         'email' : 'user@gmail.com',
-        'last_name' : 'Lastname',
+        'last_name' : 'lastname',
+    }
+
+    USER_WRONG_DATA_TEST = {
+        'first_name' : 'User',
+        'email' : 'user@gmail.com',
+        'last_name' : 'lastname',
     }
 
     def setUp(self) -> None:
 
         user = User.objects.create_user( username='userTest', password='password')
-        #user_two = User.objects.create_user( username='userTestTwo', password='password')
 
-        auth_response = self.client.post(f'{self.BASE_URL}/api/token/', data={'username': 'userTest', 'password': 'password'})
+        auth_response = self.client.post(
+            f'{self.BASE_URL}/api/token/', 
+            data={'username': 'userTest', 
+            'password': 'password'}
+        )
+
         token = auth_response.data.get('access')
 
         self.assertEqual(auth_response.status_code, 200)
         self.assertIsNotNone(token)
 
         self.user = user
+
         self.token = f'Bearer {token}'
 
-        patch_response = self.client.patch(
-            f'{self.BASE_URL}/users/{self.user_id}/',
-            data={
-                'password': 'password',
-            },
-            HTTP_AUTHORIZATION=f'Bearer {token}'
-        )
-
-        self.assertEqual(patch_response.status_code, 200)
-
-        self.user_id = patch_response.data.get('id')
-    
-    # Post tests
+    # Get tests
 
     def test_get_user(self):
         response = self.client.get(f'{self.BASE_URL}/users/')
-        print(response.data)
-        
         self.assertEqual(response.status_code, 401)
+
 
     def test_create_correctly(self):
         create_response = self.client.post(
             f'{self.BASE_URL}/users/',
             data=self.USER_DATA_TEST,
         )
-        print(create_response.data)
-
         self.assertEqual(create_response.status_code, 201)
 
-    def test_update_permission(self):
+    def test_create_whit_wrong_data(self):
+        create_response = self.client.post(
+            f'{self.BASE_URL}/users/',
+            data=self.USER_WRONG_DATA_TEST,
+        )
+        self.assertEqual(create_response.status_code, 400)
+
+    # Token tests
+
+    def test_get_token(self):
+
+        auth_response = self.client.post(
+            f'{self.BASE_URL}/api/token/', 
+            data={'username': 'userTest', 
+            'password': 'password'}
+        )
+        token = auth_response.data.get('access')
+
+        self.assertEqual(auth_response.status_code, 200)
+        self.assertIsNotNone(token)
+
+    # Patch tests
+    def test_update(self):
         update_response = self.client.patch(
-            f'{self.BASE_URL}/users/{self.user_id}/',
-            data={
-                'password': 'password',
-            },
-            HTTP_AUTHORIZATION=self.token
-    )
-        self.assertEqual(update_response.status_code, 201)
+        f'{self.BASE_URL}/users/{self.user.id}/',
+        data={
+            'username': 'visibility'
+        },
+        HTTP_AUTHORIZATION=self.token
+        )
+
+        self.assertEqual(update_response.status_code, 200)
 
